7eca1f1aa51d482cbedd82819789130808fcb83e
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-04-10 18:17:08 +0300

    implement skip link (with route changes)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
 exports.onRouteUpdate = ({ location, prevLocation }) => {
-   
+  if (prevLocation) {
+    const skipLink = document.querySelector('.routeSkipLink');
+
+    if (skipLink) { skipLink.focus(); }
+  }
 }
