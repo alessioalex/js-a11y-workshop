72e853b27257c19d2ace728dc925841e8cdb1c1e
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-04-10 17:48:06 +0300

    implement accessible dropdown

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -3,12 +3,69 @@ import uuid from "uuid"
 
 import "./dropdown.scss"
 
-const Dropdown = ({ }) => {
+const Dropdown = ({ activatorText = 'Dropdown', items = [] }) => {
+    const activatorRef = useRef(null);
+    const dropdownListRef = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const clickHandler = (event) => {
+        setIsOpen(!isOpen);
+    };
+
+    const keyHandler = (event) => {
+        if (isOpen && event.key === 'Escape') {
+            setIsOpen(false);
+        }
+    };
+
+    const clickOutsideHandler = (event) => {
+        if (dropdownListRef.current.contains(event.target) || 
+            activatorRef.current.contains(event.target)) {
+            return;
+        } else {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            dropdownListRef.current.querySelector('a').focus();
+            document.addEventListener('mousedown', clickOutsideHandler);
+        } else {
+            document.removeEventListener('mousedown', clickOutsideHandler);
+            // activatorRef.current.focus();
+        }
+    }, [isOpen]);
+
     return (
         <div
             className="dropdown-wrap"
         >
+            <button
+                aria-haspopup="true"
+                aria-controls="dropdown1"
+                onClick={clickHandler}
+                ref={activatorRef}
+                className="dropdown-activator"
+                onKeyUp={keyHandler}
+            >
+                {activatorText}
+            </button>
 
+            <ul
+                id="dropdown1"
+                ref={dropdownListRef}
+                tabIndex="-1"
+                className={`dropdown-itemList ${isOpen ? 'active' : ''}`}
+                onKeyUp={keyHandler}
+                role="list"
+            >
+                {items.map((item, index) => {
+                    return <li key={index}>
+                        <a href={item.url}>{item.text}</a>
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }
