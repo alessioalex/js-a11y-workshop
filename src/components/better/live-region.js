a7843f36bacdcc5685a1ed80ac4b1149f238ec04
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-12 01:15:07 +0300

    creating a live region component

--- src/components/better/live-region.js
+++ src/components/better/live-region.js
@@ -1,10 +1,28 @@
 import React, {useState, useRef} from "react"
 
 const LiveRegion = () => {
+  const [message, setMessage] = useState('Nothing here.');
+
+  const inputRef = useRef(null);
+
+  const submitHandler = (event) => {
+    setMessage(inputRef.current.value);
+    event.preventDefault();
+  };
 
   return (
-    <>
-    </>
+    <div>
+      <div role="status">
+        <p>{message}</p>
+      </div>
+      <form onSubmit={submitHandler}>
+        <label>
+          Enter text<br />
+          <input type="text" ref={inputRef} />
+        </label>
+        <button type="submit">Start</button>
+      </form>
+    </div>
   )
 }
 
